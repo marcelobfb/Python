8685877abd8a79733b8aee01f207b54b739d06de
Marcelo Bernardes <marcelobouchardet@hotmail.com>
2025-02-03 12:51:31 -0300

    web scraping mercado livre

--- WebScrap/Python/Web_scraping/mercardo_livre.py
+++ WebScrap/Python/Web_scraping/mercardo_livre.py
@@ -1,31 +1,35 @@
-#obtendo produtos do mercado livre a partir de uma buscara realizada pelo usuario
-import requests # type: ignore
-from bs4 import BeautifulSoup # type: ignore
-import pandas as pd # type: ignore
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-lista_produtos=[]
+lista_produtos = []
 
-p=str(input('Digite o nome do produto que deseja: '))
-url_base='https://lista.mercadolivre.com.br/'
-url_produto = url_base + p + f"#D[A:{p}]"
-response=requests.get(url_produto)
-site=BeautifulSoup(response.text,'html.parser')
-produtos=site.findAll('div',attrs={'class':'andes-card ui-search-result shops__cardStyles ui-search-result--core andes-card--flat andes-card--padding-16'})
+p = str(input('Digite o nome do produto que deseja: '))
+url_base = 'https://lista.mercadolivre.com.br/'
+url_produto = url_base + p + '#D[A:{p}]'
+response = requests.get(url_produto)
+site = BeautifulSoup(response.text, 'html.parser')
+
+# Usando a classe correta para os produtos
+produtos = site.find_all('li', attrs={'class': 'ui-search-layout__item'})
+
 for produto in produtos:
-    titulo=produto.find('a',attrs={'class':'poly-component_title'})
-    moeda=produto.find('span',attrs={'class':'andes-money-amount__currency-symbol'})
-    link=produto.find('a',attrs={'class':'ui-search-link'})
-    real=produto.find('span',attrs={'class':'andes-money-amount__fraction'})
-    centavos=produto.find('span',attrs={'class':'andes-money-amount__cents'})
-    #print(produto.prettify())
-    print(f'Titulo do produto: {titulo.text}')
-    print('O link do produto é:', link['href'])
-    if centavos:
-        print(f'{moeda.text}{real.text},{centavos.text}')
-        tabela=pd.DataFrame(lista_produtos.append([titulo.text,link['href'],moeda.text,real.text,centavos.text]))
+    titulo_element = produto.find('a', attrs={'class': 'poly-component__title'})
+    moeda_element = produto.find('span', attrs={'class': 'andes-money-amount__currency-symbol'})
+    link_element = produto.find('a', attrs={'class': 'poly-component__title'})
+    real_element = produto.find('span', attrs={'class': 'andes-money-amount__fraction'})
+    centavos_element = produto.find('span', attrs={'class': 'andes-money-amount__cents andes-money-amount__cents--superscript-24'})
+
+    if titulo_element and link_element and moeda_element and real_element:
+        titulo = titulo_element.text.strip()
+        link = link_element['href']
+        moeda = moeda_element.text.strip()
+        real = real_element.text.strip()
+        centavos = centavos_element.text.strip() if centavos_element else '00'
+        lista_produtos.append([titulo, link, moeda, real, centavos])
     else:
-        print(f'{moeda.text}{real.text},00')
-        tabela=pd.DataFrame(lista_produtos.append([titulo.text,link['href'],moeda.text,real.text,'00']))
+        print('Não foi possível encontrar todas as informações do produto.')
     print('\n\n')
-produtin=pd.DataFrame(lista_produtos,columns=['Título','Link','Moeda','Real','Centavos'])
-produtin.to_excel('produtos.xlsx',index=False)
+
+tabela = pd.DataFrame(lista_produtos, columns=['Título', 'Link', 'Moeda', 'Real', 'Centavos'])
+tabela.to_excel('produtos.xlsx', index=False)
